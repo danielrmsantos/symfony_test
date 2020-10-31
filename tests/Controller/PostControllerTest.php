271559eb9a744de39278ad68d5d46af36f7b959a
Daniel Santos <daniel.santos@webcomum.com>
2020-10-31 19:12:51 +0000

    All endpoints tested

--- tests/Controller/PostControllerTest.php
+++ tests/Controller/PostControllerTest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Tests\Controller;
 
 use App\Entity\Post;
@@ -9,52 +10,32 @@ use Symfony\Component\HttpFoundation\Response;
 class PostControllerTest extends WebTestCase
 {
     /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $entityManager;
+    
+    /**
      * @var KernelBrowser
      */
     private $client;
     
+    /**
+     * @var int
+     */
+    private $defaultPostId;
+    
+    
+    /**
+     * @var string
+     */
+    private $defaultPostChannel;
+    
     protected function setUp()
     {
         $this->client = self::createClient();
-    }
-    
-    /**
-     * Test GET Post by id endpoint
-     */
-    public function testGetPostById(): void
-    {
-        $this->client->request('GET', '/posts/1');
-        $response = $this->client->getResponse();
-        $responseData = json_decode($this->client->getResponse()->getContent(), true);
-        
-        self::assertEquals(Response::HTTP_OK, $response->getStatusCode(), 'Did not get Status Code 200');
-        self::assertSame('application/json', $response->headers->get('Content-Type'), 'Did not get application/json Content-Type in headers.');
-        self::assertNotEmpty($responseData, 'Response is empty');
-        $this->validatePost($responseData);
-    }
-    
-    /**
-     * Test GET Post by channel endpoint
-     */
-    public function testGetPostsByChannel(): void
-    {
-        $this->client->request('GET', '/posts/website');
-        $response = $this->client->getResponse();
-        $responseData = json_decode($this->client->getResponse()->getContent(), true);
-    
-        $em = $this->client->getContainer()->get('doctrine')->getManager();
-    
-        $items = $em->getRepository(Post::class)->findByChannel('website');
-        
-        self::assertEquals(Response::HTTP_OK, $response->getStatusCode(), 'Did not get Status Code 200');
-        self::assertSame('application/json', $response->headers->get('Content-Type'), 'Did not get application/json Content-Type in headers.');
-        self::assertNotEmpty($responseData, 'Response is empty');
-        self::assertGreaterThanOrEqual(1, count($responseData), 'Should get at least one Post with channel website');
-        self::assertCount(count($items), $responseData, 'Results count from response expected to be the same as from DB query');
-        
-        foreach ($responseData as $post) {
-            $this->validatePost($post);
-        }
+        $this->entityManager = $this->client->getContainer()->get('doctrine')->getManager();
+        $this->defaultPostId = 1;
+        $this->defaultPostChannel = 'website';
     }
     
     /**
@@ -65,16 +46,16 @@ class PostControllerTest extends WebTestCase
         $this->client->request('GET', '/posts/');
         $response = $this->client->getResponse();
         $responseData = json_decode($this->client->getResponse()->getContent(), true);
-    
-        $em = $this->client->getContainer()->get('doctrine')->getManager();
-    
-        $items = $em->getRepository(Post::class)->findAll();
+        
+        $items = $this->entityManager->getRepository(Post::class)->findAll();
         
         self::assertEquals(Response::HTTP_OK, $response->getStatusCode(), 'Did not get Status Code 200');
-        self::assertSame('application/json', $response->headers->get('Content-Type'), 'Did not get application/json Content-Type in headers.');
+        self::assertSame('application/json', $response->headers->get('Content-Type'),
+            'Did not get application/json Content-Type in headers.');
         self::assertNotEmpty($responseData, 'Response is empty');
         self::assertGreaterThanOrEqual(1, count($responseData), 'Should get at least one Post with channel website');
-        self::assertCount(count($items), $responseData, 'Results count from response expected to be the same as from DB query');
+        self::assertCount(count($items), $responseData,
+            'Results count from response expected to be the same as from DB query');
         
         foreach ($responseData as $post) {
             $this->validatePost($post);
@@ -82,28 +63,130 @@ class PostControllerTest extends WebTestCase
     }
     
     /**
-     * Test create a Post endpoint
+     * Test GET Post by id endpoint
      */
-    public function testCreatePost(): void
+    public function testGetPostById(): void
     {
-        $post = json_encode([
-            'title' => 'PHP Unit Testing',
-            'description' => 'Hello Hello Hello Hello Hello',
-            'channel' => 'website',
-        ]);
-        
-        $this->client->request('POST', '/posts/create', [], [], ['CONTENT_TYPE' => 'application/json'], $post);
+        $this->client->request('GET', '/posts/' . $this->defaultPostId);
         $response = $this->client->getResponse();
         $responseData = json_decode($this->client->getResponse()->getContent(), true);
         
-        self::assertEquals(Response::HTTP_CREATED, $response->getStatusCode(), 'Did not get Status Code 201');
-        self::assertSame('application/json', $response->headers->get('Content-Type'), 'Did not get application/json Content-Type in headers.');
+        self::assertEquals(Response::HTTP_OK, $response->getStatusCode(), 'Did not get Status Code 200');
+        self::assertSame('application/json', $response->headers->get('Content-Type'),
+            'Did not get application/json Content-Type in headers.');
         self::assertNotEmpty($responseData, 'Response is empty');
         $this->validatePost($responseData);
     }
     
     /**
-     * Validate that Post has expected properties and values
+     * Test GET Post by channel endpoint
+     */
+    public function testGetPostsByChannel(): void
+    {
+        $this->client->request('GET', '/posts/' . $this->defaultPostChannel);
+        $response = $this->client->getResponse();
+        $responseData = json_decode($this->client->getResponse()->getContent(), true);
+        
+        $items = $this->entityManager->getRepository(Post::class)->findByChannel($this->defaultPostChannel);
+        
+        self::assertEquals(Response::HTTP_OK, $response->getStatusCode(), 'Did not get Status Code 200');
+        self::assertSame('application/json', $response->headers->get('Content-Type'),
+            'Did not get application/json Content-Type in headers.');
+        self::assertNotEmpty($responseData, 'Response is empty');
+        self::assertGreaterThanOrEqual(1, count($responseData),
+            'Should get at least one Post with channel ' . $this->defaultPostChannel);
+        self::assertCount(count($items), $responseData,
+            'Results count from response expected to be the same as from DB query');
+        
+        foreach ($responseData as $post) {
+            $this->validatePost($post);
+        }
+    }
+    
+    /**
+     * Test Create a Post endpoint
+     * The created Post id is returned to testDeletePost function to be deleted
+     * as suggested dama/doctrine-test-bundle package throws deprecation notices
+     */
+    public function testCreatePost(): int
+    {
+        $postContent = json_encode([
+            'title' => 'PHP Unit Testing',
+            'description' => 'Hello Hello Hello Hello Hello',
+            'channel' => $this->defaultPostChannel,
+        ]);
+        
+        $this->client->request('POST', '/posts/create', [], [], ['CONTENT_TYPE' => 'application/json'], $postContent);
+        $response = $this->client->getResponse();
+        $responseData = json_decode($this->client->getResponse()->getContent(), true);
+        
+        self::assertEquals(Response::HTTP_CREATED, $response->getStatusCode(), 'Did not get Status Code 201');
+        self::assertSame('application/json', $response->headers->get('Content-Type'),
+            'Did not get application/json Content-Type in headers.');
+        self::assertNotEmpty($responseData, 'Response is empty');
+        $this->validatePost($responseData);
+        
+        return $responseData['id'];
+    }
+    
+    /**
+     * Test Update a Post endpoint
+     */
+    public function testUpdatePost(): void
+    {
+        $post = $this->entityManager->getRepository(Post::class)->findOneBy(['id' => $this->defaultPostId]);
+        $currentPostTitle = $post->getTitle();
+        
+        $title = 'PHP Unit Testing UPDATED';
+        $postContent = json_encode([
+            'title' => $title,
+        ]);
+        
+        $this->client->request('PUT', '/posts/1', [], [], ['CONTENT_TYPE' => 'application/json'], $postContent);
+        $response = $this->client->getResponse();
+        $responseData = json_decode($this->client->getResponse()->getContent(), true);
+        
+        self::assertEquals(Response::HTTP_OK, $response->getStatusCode(), 'Did not get Status Code 200');
+        self::assertSame('application/json', $response->headers->get('Content-Type'),
+            'Did not get application/json Content-Type in headers.');
+        self::assertNotEmpty($responseData, 'Response is empty');
+        $this->validatePost($responseData);
+        self::assertEquals($title, $responseData['title']);
+        
+        /*
+         * Reset updated post manually as suggested dama/doctrine-test-bundle throws deprecation notices
+         * @see https://github.com/dmaicher/doctrine-test-bundle/issues/129
+         *
+         */
+        $post->setTitle($currentPostTitle);
+        $this->entityManager->persist($post);
+        $this->entityManager->flush();
+    }
+    
+    /**
+     * Test Delete a Post endpoint
+     *
+     * Delete the created Post manually as suggested dama/doctrine-test-bundle package throws deprecation notices
+     * @see https://github.com/dmaicher/doctrine-test-bundle/issues/129
+     *
+     * @depends testCreatePost
+     *
+     * @param integer $id
+     */
+    public function testDeletePost($id): void
+    {
+        $this->client->request('DELETE', '/posts/' . $id);
+        $response = $this->client->getResponse();
+        
+        self::assertEquals(Response::HTTP_NO_CONTENT, $response->getStatusCode(), 'Did not get Status Code 204');
+        self::assertEmpty($this->client->getResponse()->getContent(), 'Response is not empty');
+    
+        $post = $this->entityManager->getRepository(Post::class)->findOneBy(['id' => $id]);
+        self::assertEmpty($post);
+    }
+    
+    /**
+     * Validate that Post has expected attributes and values
      *
      * @param $post
      */
@@ -117,5 +200,14 @@ class PostControllerTest extends WebTestCase
         self::assertIsString($post['title'], 'Got a ' . gettype($post['title']) . ' instead of string');
         self::assertIsString($post['description'], 'Got a ' . gettype($post['description']) . ' instead of string');
         self::assertIsString($post['channel'], 'Got a ' . gettype($post['channel']) . ' instead of string');
+    }
+    
+    protected function tearDown()
+    {
+        parent::tearDown();
+        
+        // doing this is recommended to avoid memory leaks
+        $this->entityManager->close();
+        $this->entityManager = null;
     }
 }
--- tests/Repository/PostRepositoryTest.php
+++ tests/Repository/PostRepositoryTest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Tests\Repository;
 
 use App\Entity\Post;
@@ -11,23 +12,67 @@ class PostRepositoryTest extends KernelTestCase
      */
     private $entityManager;
     
+    /**
+     * @var int
+     */
+    private $defaultPostId;
+    
+    
+    /**
+     * @var string
+     */
+    private $defaultPostChannel;
+    
     protected function setUp()
     {
         $kernel = self::bootKernel();
         
-        $this->entityManager = $kernel->getContainer()
-            ->get('doctrine')
-            ->getManager();
+        $this->entityManager = $kernel->getContainer()->get('doctrine')->getManager();
+        $this->defaultPostId = 1;
+        $this->defaultPostChannel = 'website';
     }
     
-    public function testSearchById()
+    /**
+     * Find a Post by id
+     */
+    public function testFindById(): void
     {
-        $post = $this->entityManager
-            ->getRepository(Post::class)
-            ->findOneBy(['id' => 1])
-        ;
+        $post = $this->entityManager->getRepository(Post::class)->findOneBy(['id' => $this->defaultPostId]);
         
-        $this->assertSame(1, $post->getId());
+        self::assertSame($this->defaultPostId, $post->getId());
+        self::assertInstanceOf(Post::class, $post);
+        $this->validatePost($post);
+    }
+    
+    /**
+     * Find a Post by channel
+     */
+    public function testFindByChannel()
+    {
+        $posts = $this->entityManager->getRepository(Post::class)->findByChannel($this->defaultPostChannel);
+    
+        self::assertNotEmpty($posts, 'No Posts found on given channel: ' . $this->defaultPostChannel);
+    
+        foreach ($posts as $post) {
+            $this->validatePost($post);
+        }
+    }
+    
+    /**
+     * Validate that Post has expected attributes and values
+     *
+     * @param Post $post
+     */
+    private function validatePost(Post $post): void
+    {
+        self::assertObjectHasAttribute('id', $post);
+        self::assertObjectHasAttribute('title', $post);
+        self::assertObjectHasAttribute('description', $post);
+        self::assertObjectHasAttribute('channel', $post);
+        self::assertIsInt($post->getId(), 'Got a ' . gettype($post->getId()) . ' instead of integer');
+        self::assertIsString($post->getTitle(), 'Got a ' . gettype($post->getTitle()) . ' instead of string');
+        self::assertIsString($post->getDescription(), 'Got a ' . gettype($post->getDescription()) . ' instead of string');
+        self::assertIsString($post->getChannel(), 'Got a ' . gettype($post->getChannel()) . ' instead of string');
     }
     
     protected function tearDown()
